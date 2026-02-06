07f908c2203bc1979864afbe74f4331caaa46a34
Ahmad Hajjouz <ahmad.hajjouz@glueckkanja.com>
2026-02-06 16:58:54 +0100

    20260206hostname

--- configuration profiles/setHostname.sh
+++ configuration profiles/setHostname.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Get the device's serial number
-SERIAL_NUMBER=$(dmidecode -s system-serial-number)
+SERIAL_NUMBER=$(sudo dmidecode -s system-serial-number)
 
 # Define the new device name with the serial number appended
 NEW_HOSTNAME="C-$SERIAL_NUMBER"
 
 # Change the hostname
-hostnamectl set-hostname $NEW_HOSTNAME
+sudo hostnamectl set-hostname $NEW_HOSTNAME
 
 # Update /etc/hostname
-echo $NEW_HOSTNAME | tee /etc/hostname
+echo $NEW_HOSTNAME | sudo tee /etc/hostname
 
 # Update /etc/hosts
 CURRENT_HOSTNAME=$(hostname)
-sed -i "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
+sudo sed -i "s/$CURRENT_HOSTNAME/$NEW_HOSTNAME/g" /etc/hosts
